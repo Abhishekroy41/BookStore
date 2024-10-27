404d906a0a346a567087f789678d4900039c305a
Abhishek <abhiroy8986@gmail.com>
2024-10-27 21:18:35 +0530

    update backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,25 @@
-import express from "express"
-import dotenv from "dotenv"
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const app = express();
 
 dotenv.config();
 
 const PORT=process.env.PORT || 4000;
+const URI=process.env.MongoDBURI;
 
-app.get('/', (req, res) => {
-  res.send('Bookstore!')
-})
+//connect to mongoDb
+try {
+  mongoose.connect(URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  console.log("connect to mongoDB");
+
+} catch (error) {
+  console.log("Error: ",error)
+  
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
